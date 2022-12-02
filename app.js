581da5ad60f7f55908b94103a0c10e3faee5e1a8
VeroniqueJ <119684515+VeroniqueJ@users.noreply.github.com>
2022-12-03 00:52:05 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let radius = 10;
+let areaResult = Math.PI * (radius * radius);
+console.log(areaResult);
+
+let phonePrice = 119.95;
+let allPhones = phonePrice * 30;
+//console.log(allPhones);
+let taxPrice = ((allPhones / 100) * 5);
+//console.log(taxPrice);
+let totalPrice = allPhones + taxPrice;
+console.log(totalPrice)
