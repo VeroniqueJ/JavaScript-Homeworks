9a38095a4174a16d6e9474b1602aeef568a303af
VeroniqueJ <119684515+VeroniqueJ@users.noreply.github.com>
2022-12-06 17:04:58 +0100

    Add files via upload

---
+++ JC Homework 2/Chinese Zodiac/app.js
@@ -0,0 +1,27 @@
+let birthYear = prompt ("What is your birth year?");
+let remainder = (parseFloat(birthYear) - 4) % 12;
+if(remainder = 0){
+    console.log(alert('Your birth year is in the sign of RAT'))
+} else if (remainder = 1){
+    console.log(alert('Your birth year is in the sign of OX'))
+} else if (remainder = 2){
+    console.log(alert('Your birth year is in the sign of TIGER'))
+} else if(remainder = 3){
+    console.log(alert('Your birth year is in the sign of RABBIT'))
+}else if(remainder = 4){
+    console.log(alert('Your birth year is in the sign of DRAGON'))
+}else if(remainder = 5){
+    console.log(alert('Your birth year is in the sign of SNAKE'))
+}else if(remainder = 6){
+    console.log(alert('Your birth year is in the sign of HORSE'))
+}else if(remainder = 7){
+    console.log(alert('Your birth year is in the sign of GOAT'))
+}else if(remainder = 8){
+    console.log(alert('Your birth year is in the sign of MONKEY'))
+}else if(remainder = 9){
+    console.log(alert('Your birth year is in the sign of ROOSTER'))
+}else if(remainder = 10){
+    console.log(alert('Your birth year is in the sign of DOG'))
+}else if(remainder = 11){
+    console.log(alert('Your birth year is in the sign of PIG'))
+}
---
+++ JC Homework 2/Friday Cash/app.js
@@ -0,0 +1,11 @@
+let firstNumber = prompt("Amount of Money in my pocket:");
+if(firstNumber >= 50){
+    console.log(alert('You should go out to a dinner and a movie.'))
+} else if(firstNumber >= 35) {
+    console.log(alert('You should go to a fine meal'))
+} else if(firstNumber>= 12){
+    console.log(alert('You should go see a movie'))
+    }
+else {
+    console.log(alert('Looks like you\'l be watching TV'))
+}
